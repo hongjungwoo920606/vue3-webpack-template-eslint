3f971beeb3b224cc7061ec882a0123b4a9c4a390
hongjungwoo920606 <jysung50@naver.com>
2021-06-21 21:56:40 +0900

    start project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ message }}</h1>
+  <HelloWorld />
+</template>
+
+<script>
+ import HelloWorld from '~/components/Helloworld'
+
+ export default {
+     components: {
+         HelloWorld
+     },
+
+     data() {
+         return {
+             message: 'hello Vue!!!'
+         }
+     }
+ }
+</script>
